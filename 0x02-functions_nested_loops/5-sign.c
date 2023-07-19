56581639184d665ebdc531b48be560ef22122b90
fahmi <fahmimohammed3727@gmail.com>
2023-07-19 03:32:50 -0700

    pno

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * print_sign - determines if the input is
+ * positve, negative or zero
+ *
+ * @n: input is an integer
+ *
+ * Return: 1 is positive, 0 is zero, -1 is negatve
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar(43);
+		return (1);
+	}
+	else if (n < 0)
+	{
+		_putchar(45);
+		return (-1);
+	}
+	else
+	{
+		_putchar(48);
+	}
+	_putchar('\n');
+}
